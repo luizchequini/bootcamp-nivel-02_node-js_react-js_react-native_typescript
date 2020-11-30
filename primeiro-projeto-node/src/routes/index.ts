aedf03fa6d8fb323fa21c98ae80abfdc32564e3d
Luiz Chequini <luizchequini@gmail.com>
2020-11-29 22:05:30 -0300

    Estrutura e padrões - Configurando Prettier

--- primeiro-projeto-node/src/routes/index.ts
+++ primeiro-projeto-node/src/routes/index.ts
@@ -2,6 +2,8 @@ import { Router } from 'express';
 
 const routes = Router();
 
-routes.get('/', (request, response) => response.json({ message: 'Hellow Luiz Chequini ;)' }));
+routes.get('/', (request, response) =>
+    response.json({ message: 'Hellow Luiz Chequini ;)' }),
+);
 
 export default routes;
