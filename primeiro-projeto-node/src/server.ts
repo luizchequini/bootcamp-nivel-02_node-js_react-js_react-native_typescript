4be944ce7c2deed401d54fe703561c68d144ca54
Luiz Chequini <luizchequini@gmail.com>
2020-11-29 21:02:38 -0300

    Estrutura e padrões - Configurando ESLint

--- primeiro-projeto-node/src/server.ts
+++ primeiro-projeto-node/src/server.ts
@@ -1,11 +1,9 @@
-import express, { request, response } from 'express';
+import express from 'express';
 
 const app = express();
 
-app.get('/', (request, response)=>{
-    return response.json({message:'Hellow Luiz Chequini ;)'});
-});
+app.get('/', (request, response) => response.json({ message: 'Hellow Luiz Chequini ;)' }));
 
-app.listen(3333, ()=>{
-    console.log('Server started on port 3333!');
+app.listen(3333, () => {
+  console.log('Server started on port 3333!');
 });
